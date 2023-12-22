0468e95d7696d788a9b1979d26ddcd097e8ea7de
itsmeajay1699 <ajayroy03377@gmail.com>
2023-12-22 22:02:57 +0530

    origin changes

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(passport.initialize());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://social-media-plateform.vercel.app/",
     credentials: true,
   })
 );
